04a06d7e3dfd41d831fa4980adc1aa46262619cc
tejascoding <youngmindsofindia@gmail.com>
2017-04-07 17:27:24 +0530

    Add files via upload

---
+++ seleniumExample/explicitWait.py
@@ -0,0 +1,15 @@
+# Explicit Waits
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.co.in/url_that_delays_loading")
+try:
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
+
+finally:
+    driver.quit()
